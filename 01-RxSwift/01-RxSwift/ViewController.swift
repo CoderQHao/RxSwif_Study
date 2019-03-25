3b348da7d964c7288581c8bec329265bc44d6511
Qing ’s <haoqing3059@icloud.com>
2019-03-25 11:10:44 +0800

    RxSwift

--- 01-RxSwift/01-RxSwift/ViewController.swift
+++ 01-RxSwift/01-RxSwift/ViewController.swift
@@ -25,10 +25,11 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "musicCell")
         
         /*
-         DisposeBag：作用是 Rx 在视图控制器或者其持有者将要销毁的时候，自动释法掉绑定在它上面的资源。它是通过类似“订阅处置机制”方式实现（类似于 NotificationCenter 的 removeObserver）。
+         DisposeBag：作用是 Rx 在视图控制器或者其持有者将要销毁的时候，自动释放掉绑定在它上面的资源。它是通过类似“订阅处置机制”方式实现（类似于 NotificationCenter 的 removeObserver）。
          rx.items(cellIdentifier:）:这是 Rx 基于 cellForRowAt 数据源方法的一个封装。传统方式中我们还要有个 numberOfRowsInSection 方法，使用 Rx 后就不再需要了（Rx 已经帮我们完成了相关工作）。
          rx.modelSelected： 这是 Rx 基于 UITableView 委托回调方法 didSelectRowAt 的一个封装。
          */
+        
         musicListViewModel.data.bind(to: tableView.rx.items(cellIdentifier: "musicCell")) { (_, music, cell) in
             cell.textLabel?.text = music.name
             cell.detailTextLabel?.text = music.singer
